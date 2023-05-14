01cce107788c3e5f5cfb9a522e40293f0d211ad8
pratikgrv <pratikgaurav37@gmail.com>
2023-05-15 00:18:06 +0530

    played around

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
+// Import required modules
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
 
-
-
+// Create a new Express app instance
 const app = express();
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// Configure view engine and view directory
+app.engine('ejs', ejsMate); // use ejsMate as the template engine
+app.set('view engine', 'ejs'); // set ejs as the default template engine
+app.set('views', path.join(__dirname, 'views')); // set the views directory to ./views
+
+// Serve static files from the /public directory
 app.use(express.static(__dirname + '/public'));
 
-
+// Define routes
 app.get('/', (req, res) => {
-    res.render('home')
+  res.render('home'); // render the home view
 });
 
-app.get('/about',(req, res) => {
-    res.render('about')
+app.get('/about', (req, res) => {
+  res.render('about'); // render the about view
 });
 
-app.get('*',(req, res) => {
-    res.render('notfound')
+// Handle all other routes with the notfound view
+app.get('*', (req, res) => {
+  res.render('notfound'); // render the notfound view
 });
 
-
-
+// Start the server and listen on port 3000
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+  console.log('Serving on port 3000');
+});
